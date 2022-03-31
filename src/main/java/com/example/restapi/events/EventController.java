4815d4073b158b075941120a2137c5dc3ff88982
ddgg9511@naver.com <ddgg9511@naver.com>
2022-03-31 21:03:32 +0900

    query events

--- src/main/java/com/example/restapi/events/EventController.java
+++ src/main/java/com/example/restapi/events/EventController.java
@@ -2,12 +2,18 @@ package com.example.restapi.events;
 
 import com.example.restapi.common.ErrorResource;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,6 +61,14 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createUri).body(eventResource);
+    }
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        var entityModels = assembler.toModel(page, e -> new EventResource(e));
+        entityModels.add(Link.of("/docs/index.html#resources-evens-list").withRel("profile"));
+        return ResponseEntity.ok(entityModels);
     }
 
     private ResponseEntity<ErrorResource> badRequest(Errors errors) {
