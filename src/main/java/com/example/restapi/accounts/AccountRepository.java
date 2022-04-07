72034fc3f6be8c68fd5e2c02e0c9c7776401002d
dddgg9511 <ddgg9511@naver.com>
2022-04-08 00:02:19 +0900

    Spring Security 적용

--- src/main/java/com/example/restapi/accounts/AccountRepository.java
+++ src/main/java/com/example/restapi/accounts/AccountRepository.java
@@ -2,5 +2,8 @@ package com.example.restapi.accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Accounts, Integer> {
+    Optional<Accounts> findByEmail(String email);
 }
--- src/main/java/com/example/restapi/accounts/AccountService.java
+++ src/main/java/com/example/restapi/accounts/AccountService.java
@@ -1,15 +1,33 @@
 package com.example.restapi.accounts;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
+    private final AccountRepository accountRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Accounts accounts = this.accountRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new User(accounts.getEmail(), accounts.getPassword(), authorities(accounts.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority(r.name()))
+                .collect(Collectors.toSet());
     }
 }
